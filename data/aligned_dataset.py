Fixed RSC normalization issue

data/aligned_dataset.py
@@ -26,8 +26,11 @@ class AlignedDataset(BaseDataset):
     def __getitem__(self, index):
         AB_path = self.AB_paths[index]
         AB = Image.open(AB_path).convert('RGB')
-        # AB = AB.resize(
-        #     (self.opt.loadSizeX * 2, self.opt.loadSizeY), Image.BICUBIC)
+        
+        # DAVID[1]: Sample image height from ~N(360,100), by default
+        resizeY = int(np.random.normal(self.opt.loadSizeY, self.opt.loadSizeYSigma))
+        resizeY = max(self.opt.fineSize, resizeY) # lower bound
+        
         # AARON: change to keep original aspect ratio, use square crop afterwards
         # by default self.opt.loadSizeY = 360 < self.opt.loadSizeX,
         # if the image is in potrait, scale the shorter edge to 360 * 2 = 720
@@ -35,39 +38,26 @@ class AlignedDataset(BaseDataset):
         if width / 2 > height:
             aspect_ratio = width / height
             AB = AB.resize(
-                (int(aspect_ratio * self.opt.loadSizeY), self.opt.loadSizeY),
+                (int(aspect_ratio * resizeY), resizeY),
                 Image.BICUBIC)
         else:
             aspect_ratio = height / width
             AB = AB.resize(
-                (self.opt.loadSizeY * 2, int(aspect_ratio * self.opt.loadSizeY * 2)),
+                (resizeY * 2, int(aspect_ratio * resizeY * 2)),
                 Image.BICUBIC)
 
-        w_total = AB.width
+        AB = self.transform(AB)
+
+        w_total = AB.size(2)
         w = int(w_total / 2)
-        h = AB.height
+        h = AB.size(1)
+        w_offset = random.randint(0, max(0, w - self.opt.fineSize - 1))
+        h_offset = random.randint(0, max(0, h - self.opt.fineSize - 1))
 
-        # DAVID[1]: Let's randomly sample h/w dimension here
-        patch_size = int(np.random.normal(self.opt.fineSize, self.opt.fineSizeSigma))
-        patch_size = min(min(w,h)-1, patch_size)                # upper bound
-        patch_size = max(int(self.opt.fineSize/2), patch_size)  # lower bound
-
-        # DAVID[1]: Now, we can get the location and crop the image to to it
-        w_offset = random.randint(0, max(0, w - patch_size - 1))
-        h_offset = random.randint(0, max(0, h - patch_size - 1))
-        
-        # DAVID[1]: Finally, crop and resize to original input dimension
-        A = AB.crop((w_offset, h_offset, 
-                     w_offset+patch_size, h_offset+patch_size))
-        A = A.resize((self.opt.fineSize, self.opt.fineSize), Image.BICUBIC)
-
-        B = AB.crop((w+w_offset, h_offset, 
-                     w+w_offset+patch_size, h_offset+patch_size))
-        B = B.resize((self.opt.fineSize, self.opt.fineSize), Image.BICUBIC)
-        
-        # PIL -> Tensor
-        A = self.transform(A)
-        B = self.transform(B)
+        A = AB[:, h_offset:h_offset + self.opt.fineSize,
+               w_offset:w_offset + self.opt.fineSize]
+        B = AB[:, h_offset:h_offset + self.opt.fineSize,
+               w + w_offset:w + w_offset + self.opt.fineSize]
 
         if (not self.opt.no_flip) and random.random() < 0.5:
             idx = [i for i in range(A.size(2) - 1, -1, -1)]